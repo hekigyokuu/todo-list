dafb4962dcbd11f3533c2d3956f4a56ed00d9497
Genrevvv <genrevvaguilar@gmail.com>
2025-03-20 10:36:50 +0800

    Implement /register route

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
@@ -23,12 +24,13 @@ app.get("/", async (req, res) => {
     let username = "Jinwoo";
     req.session.user = {};
 
-    
+    /*
     req.session.user.username = username;
     console.log("Welcome " + username);
 
     // RECREATE USER DATA EVERY RESTART (For testing)
     // Hardcoded data for testing
+    
     const data = {
         username: "Jinwoo",
         points: 0,
@@ -39,7 +41,7 @@ app.get("/", async (req, res) => {
     }
     await deleteData(client, "todolist", "todolist", {}, true); // Clear database
     await insertData(client, "todolist", "todolist", data);     // Insert user to database
-    
+    */
 
     if (req.session.user.username) {
         res.redirect("index.html");
@@ -50,15 +52,46 @@ app.get("/", async (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "public"))); // Allow access to static files from "public" folder
-app.use(express.json()); // Parse incoming JSON data from client
+app.use(express.urlencoded({ extended: true })); // Parse form data from client   
+app.use(express.json()); // Parse JSON data from client
 
 app.post("/login", (req, res) => {
     res.redirect("index.html");
 });
 
-app.post("/register", (req, res) => {
-    res.redirect("html/login.html");
-})
+app.post("/register", async (req, res) => {
+    const { username, password, confirmation } = req.body;
+    let userData = await getData(client, "todolist", "todolist", { username: username });
+
+    if (password !== confirmation) {
+        console.log("Registration Failed: Password confirmation failed."); 
+        res.redirect("html/register.html");
+        return;
+    }
+
+    
+    if (userData.length === 0) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        
+        // pre-determined task data are just for testing
+        const data = {
+            username: username,
+            password: hashedPassword,
+            tasks: {
+                "task1": { completed: false, reward: 5 },
+                "task2": { completed: false, reward: 10 }
+            }
+        };
+
+        await insertData(client, "todolist", "todolist", data);
+
+        res.redirect("html/login.html");
+        return;
+    }
+    
+    console.log("User data already exist");
+    res.redirect("html/register.html");
+});
 
 app.get("/get-data", async (req, res) => {
     let filter = { username: req.session.user.username };
@@ -67,9 +100,10 @@ app.get("/get-data", async (req, res) => {
 
     if (userData && userData.length > 0) {
         res.json(userData[0]);
-    } else {
-        res.status(404).json({ message: "No data found for the user" });
-    }
+        return;
+    } 
+
+    res.status(404).json({ message: "No data found for the user" });
 });
 
 app.post("/completed", async (req, res) => {
@@ -83,22 +117,21 @@ app.post("/completed", async (req, res) => {
 
     if (userData.tasks[id].completed) {
         res.json({});
+        return;
     }
-    else {
-        userData.points += userData.tasks[id].reward;
-        userData.tasks[id].completed = true;
     
-        let update = { 
-            $set: { points: userData.points, tasks: userData.tasks}
-        };
+    userData.points += userData.tasks[id].reward;
+    userData.tasks[id].completed = true;
+
+    let update = { 
+        $set: { points: userData.points, tasks: userData.tasks}
+    };
+
+    await updateData(client, "todolist", "todolist", filter, update);
     
-        await updateData(client, "todolist", "todolist", filter, update);
-        
-        console.log("Data updated");
-    
-        res.json(userData);
-    
-    }
+    console.log("Data updated");
+
+    res.json(userData);
 });
 
 app.listen(port, (err) => {
